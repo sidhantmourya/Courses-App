0a3981b2402f85afd49eb82c85d4311604be3aec
Anmol Pal <anmol111pal@gmail.com>
2025-05-18 12:16:03 +0530

    Modified Instructor entity to ommit instructor subfield in courses field

--- src/main/java/com/courseapp/entities/Instructor.java
+++ src/main/java/com/courseapp/entities/Instructor.java
@@ -1,12 +1,14 @@
 package com.courseapp.entities;
 
 import java.util.List;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -14,6 +16,7 @@ import lombok.NonNull;
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Instructor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +30,7 @@ public class Instructor {
 	private String instructorEmail;
 	
 	@OneToMany(mappedBy = "instructor")
+	@JsonIgnoreProperties("instructor")
 	@NonNull
 	private List<Course> courses;
 	
